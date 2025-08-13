607a8d7a1063cdebd42b175e5e3bdac077fdd3aa
TayZ1030 <168438191+TayZ1030@users.noreply.github.com>
2025-08-13 09:29:38 -0300

    .

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
